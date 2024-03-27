ff23e352c9279636db1835b0a0fe9824255a5bfe
KimDoYeon13 <bbs00339@gmail.com>
2024-03-28 01:12:52 +0900

    first upload

---
+++ section05/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <h1></h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>안녕 리액트!!</h1>
+    </>
+  );
+}
+
+export default App;
